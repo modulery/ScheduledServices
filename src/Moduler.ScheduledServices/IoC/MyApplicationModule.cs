5d7b1a200345b829f20b6da18396d83e6113fb57
mesuttasci <mesuttascic@gmail.com>
2024-06-21 04:29:08 +0300

    listInterfacesNMethods

--- src/Moduler.ScheduledServices/IoC/MyApplicationModule.cs
+++ src/Moduler.ScheduledServices/IoC/MyApplicationModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using RestSharp;
 using System.Reflection;
 
 namespace Moduler.ScheduledServices.IoC
@@ -6,6 +7,7 @@ namespace Moduler.ScheduledServices.IoC
     public class MyApplicationModule : Autofac.Module
     {
         public string[] AssemblyNameSpaces { get; set; }
+        public static Type[] MyInterfaces { get; private set; } = new Type[0];
         protected override void Load(ContainerBuilder builder)
         {
             var projectDirectory = AppDomain.CurrentDomain.BaseDirectory;
@@ -17,6 +19,22 @@ namespace Moduler.ScheduledServices.IoC
                 {
                     var assembly = Assembly.LoadFrom(assemblyPath);
                     Console.WriteLine(assembly.FullName);
+
+                    Type[] types = assembly.GetTypes();
+                    Type[] interfaces = types.Where(t => t.IsInterface).ToArray();
+                    if (interfaces.Length>0)
+                    {
+                        MyInterfaces = (Type[])MyInterfaces.Concat(interfaces).ToArray();
+                        foreach (Type interfaceType in interfaces)
+                        {
+                            Console.WriteLine(interfaceType.Name);
+                            foreach (var method in interfaceType.GetMethods())
+                            {
+                                var returnType = method.ReturnType.FullName;
+                                var parameters = method.GetParameters();
+                            }
+                        }
+                    }
 
                     builder.RegisterAssemblyTypes(assembly)
                         .Where(t => t.Namespace.StartsWith(assemblyName) && t.IsInterface)
